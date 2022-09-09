98c591e150a841980473a7aba902879ddda330df
Oladanniel <Olasupoddaniel1999@gmail.com>
2022-09-08 18:40:43 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
 /**
- * main - Betty 
+ * main - print the string in the put function 
  *
- *Return: Always 0 (success)
+ * Decription: using the main function
+ * this program prints "Progamming is like building a multilingual puzzle
+ *Return: 0 
  */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
+
